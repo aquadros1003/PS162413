149692d7b1d7c327b8f6e11350c316a1ac2b2c43
Jankowski Łukasz <73948593+aquadros1003@users.noreply.github.com>
2021-02-24 00:13:50 +0100

    Add files via upload

---
+++ lista1/8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,c;
+   scanf("%d\n%d\n%d",&a,&b,&c);
+   printf("%f",(a+b+c)/3.0);
+    return 0;
+}
---
+++ lista1/9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   unsigned int a;
+   scanf("%d",&a);
+   printf("%f",sqrt(a));
+    return 0;
+}
