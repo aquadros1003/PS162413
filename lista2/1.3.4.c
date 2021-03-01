eae7974403a3365b43699a0ee133ce6cbe9955c5
Jankowski Łukasz <73948593+aquadros1003@users.noreply.github.com>
2021-03-01 14:36:24 +0100

    cw2

---
+++ lista2/1.3.4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a, b;
+	scanf("%d\n%d", &a, &b);
+if (abs(a) > abs(b))
+{
+	printf("%d", a);
+}
+else
+{
+	printf("%d", b);
+}
+return 0;
+}
