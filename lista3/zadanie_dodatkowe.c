f92d732522e6dae0e43ea2431e7e0f2bf4c224e8
Jankowski Łukasz <jankowski.lukasz0987@gmail.com>
2021-03-22 13:47:27 +0100

    poprawione

--- lista3/zadanie_dodatkowe.c
+++ lista3/zadanie_dodatkowe.c
@@ -7,7 +7,7 @@ int a = 162413; //tu wpisz swoj numer indeksu
 int b=a%12; 
 int c=b%7; 
 int d= (b+=4)-(c*=2); // b=9 , c= 10, d=-1
-c=(d^=2); // b=9 , c=-1 , d=-1
-d=(b/=4)*(c-=3); // b=2 , c=-4 , d=8
+c=(d^=2); // b=9 , c=-3 , d=-3
+d=(b/=4)*(c-=3); // b=2 , c=-6 , d=-12
 return 0;
 }
