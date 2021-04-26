958edd4df283ce04fb846a84d147d5e34f0408e3
Jankowski Łukasz <jankowski.lukasz0987@gmail.com>
2021-04-26 13:33:40 +0200

    Update main.c

--- kolokwium_5/main.c
+++ kolokwium_5/main.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+int *kolokwium_5(int n, int tab)
+{
+	int* wsk;
+	for (int i = 0; i < n; i++)
+	{
+		if (tab[i] % 2 == 0)
+			*wsk = tab[i];
+		return *wsk;
+		break;
+	}
+	*wsk = tab[0]
+		return *wsk;
+}
